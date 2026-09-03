ae107c9edcb278687737ec92e00b37e46d5729a4
Gokul <gokulkv2005@gmail.com>
2026-09-03 18:07:01 +0530

    Implement lambda function to retrieve products
    Added a lambda function to fetch products from DynamoDB and convert Decimal values to JSON serializable types.

--- backend/get-products/lambda_function.py
+++ backend/get-products/lambda_function.py
@@ -1,1 +1,36 @@
+import json
+import boto3
+from decimal import Decimal
 
+dynamodb = boto3.resource("dynamodb", region_name="ap-south-1")
+table = dynamodb.Table("CloudCart-Products")
+
+
+def convert_decimal(obj):
+    if isinstance(obj, list):
+        return [convert_decimal(item) for item in obj]
+
+    if isinstance(obj, dict):
+        return {key: convert_decimal(value) for key, value in obj.items()}
+
+    if isinstance(obj, Decimal):
+        return int(obj) if obj % 1 == 0 else float(obj)
+
+    return obj
+
+
+def lambda_handler(event, context):
+
+    response = table.scan()
+
+    products = convert_decimal(response["Items"])
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "products": products
+        })
+    }
